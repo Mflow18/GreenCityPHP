83663c9a344dee58141ee29b2b023ea1029a6c3c
Mathieu Florentin <mathieu.florentin27@gmail.com>
2019-09-20 09:22:58 +0200

    mathieumergerickastley

---
+++ rickastley.php
@@ -0,0 +1,5 @@
+
+
+<?php echo 'Coucou toi, oui, toi :' . $_GET['Name'] .'qui a un super email :'. $_GET['Email'] .'et un message sympa :' . $_GET['Message'] . 'tu viens de te faire Rick Astley ! Enjoy !'; ?>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?start=85" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
